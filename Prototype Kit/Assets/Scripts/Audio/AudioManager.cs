e13177008221acf7ccc1c95276dbc0fb82aa1541
DanielTheNerd35 <vazquezd8@montclair.edu>
2026-02-10 13:56:40 -0500

    Added Sounds

---
+++ Prototype Kit/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+        
+    }
+
+    void start(){
+        
+    }
+
+   public void Play (string name)
+   {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "Not Found!");
+            return;
+        }
+        s.source.Play();
+   }
+
+   public void Stop (string name)
+   {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "Not Found!");
+            return;
+        }
+        s.source.Stop();
+   }
+}
